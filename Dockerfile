c28c322fd18430c349d8ed0f06f59179717f5acf
Izzy <undermybrella@abimon.org>
2021-09-22 15:33:18 +1000

    Initial setup (after many hours of work)

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM postgres:12-buster
+
+# Install our dependencies --
+
+RUN set -eux; \
+        apt-get update; \
+        apt-get -y install pgbackrest; \
+        rm -rf /var/lib/apt/lists/*;
+
+# Create and own our new pgbackrest dirs
+RUN set -eux; \
+        mkdir -p -m 770 /var/log/pgbackrest; \
+        chown postgres:postgres /var/log/pgbackrest; \
+        mkdir -p /etc/pgbackrest; \
+        mkdir -p -m 770 /etc/pgbackrest/conf.d; \
+        touch /etc/pgbackrest/pgbackrest.conf; \
+        chmod 640 /etc/pgbackrest/pgbackrest.conf; \
+        chown postgres:postgres /etc/pgbackrest/pgbackrest.conf; \
+        chown postgres:postgres /etc/pgbackrest/conf.d;
+
+ENV PGBACKREST_REPO /var/lib/pgbackrest/data
+RUN mkdir -p "$PGBACKREST_REPO" && chown -R postgres:postgres "$PGBACKREST_REPO" && chmod 777 "$PGBACKREST_REPO"
+VOLUME /var/lib/pgbackrest/data
+
+COPY pgbackrest-init.sh /docker-entrypoint-initdb.d/
+RUN chmod a+rx /docker-entrypoint-initdb.d/pgbackrest-init.sh
---
+++ pgbackrest-init.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'PGBACKREST_STANZA'
+file_env 'PGBACKREST_REPO' '/var/lib/pgbackrest/data'
+file_env 'PGBACKREST_CIPHER_PASS'
+file_env 'PGBACKREST_CIPHER_TYPE' 'aes-256-cbc'
+file_env 'PGBACKREST_COMPRESS_TYPE' 'zst'
+file_env 'PGBACKREST_COMPRESS_LEVEL' '6' #pgbackrest says that this is allowed between 0-9, so we pick an approximate value that should line up with our other settings
+file_env 'PGBACKREST_RETENTION_FULL' '1'
+file_env 'PGBACKREST_RETENTION_DIFF' '7'
+
+PGBACKREST_CONF="/etc/pgbackrest/conf.d/default.conf"
+POSTGRES_CONF="$PGDATA/postgresql.conf"
+
+# Check if file is not empty
+if [ ! -s $PGBACKREST_CONF ]; then
+    tee $PGBACKREST_CONF << EOF  
+[$PGBACKREST_STANZA]
+pg1-path=$PGDATA
+
+repo1-cipher-pass=$PGBACKREST_CIPHER_PASS
+repo1-cipher-type=$PGBACKREST_CIPHER_TYPE
+repo1-path=$PGBACKREST_REPO
+repo1-retention-full=$PGBACKREST_RETENTION_FULL
+repo1-retention-diff=$PGBACKREST_RETENTION_FULL
+
+[$PGBACKREST_STANZA:archive-push]
+compress-type=$PGBACKREST_COMPRESS_TYPE
+compress-level=$PGBACKREST_COMPRESS_LEVEL
+EOF
+fi
+
+# Don't try and write if the config is read only
+[ -w $POSTGRES_CONF ] && tee $POSTGRES_CONF << EOF
+archive_command = 'pgbackrest --stanza=$PGBACKREST_STANZA archive-push %p'
+archive_mode = on
+max_wal_senders = 3
+EOF
+
+pgbackrest --stanza=$PGBACKREST_STANZA --log-level-console=info stanza-create
+
+# Check that everything has gone correctly
+pgbackrest --stanza=$PGBACKREST_STANZA --log-level-console=info check
